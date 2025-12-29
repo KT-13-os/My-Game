c80d96bbf04fca4c6909c41f223d9536306058bb
K.T <2023s0080@shiba-kokusai.ed.jp>
2025-12-29 20:57:35 +0900

    Glaseのバグの修正

--- Assets/Scripts/Player/Glazecircle.cs
+++ Assets/Scripts/Player/Glazecircle.cs
@@ -9,25 +9,28 @@ public class Glazecircle : MonoBehaviour
     private GameManager _GameManager;
     private float _count;
     private float _Time;
+    private Rigidbody2D _rigidbody2D;
     void Start()
     {
         _Time = 0.08f;
         _count = 0;
         _GameManager = _gamemanager.GetComponent<GameManager>();
+        _rigidbody2D=gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _count += Time.deltaTime;
         if (transform.position == _player.transform.position) return;
         transform.position = _player.transform.position;
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
+        _count += Time.deltaTime;
         if (_count < _Time) return;
         if (collision.gameObject.tag == "bullet")
         {
+            _rigidbody2D.WakeUp();
             _GameManager.Glaze(1);
             _count=0;
         }
